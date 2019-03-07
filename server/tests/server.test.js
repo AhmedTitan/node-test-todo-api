1fe2bc48636c17234c6d9ea463df67199f1104da
Ahmed <ahmedtitan@gmail.com>
2019-03-07 19:58:40 +0530

    add tests for DELETe /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -348,3 +348,23 @@ describe('POST /users/login', () => {
             });
     });
 });
+describe('DELETE /users/me/token', (done) => {
+    it('should remove auth token remove', () => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.message).toBe('loged out.');
+            })
+            .end((err) => {
+                if(err){
+                    return done(err);
+                }
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
